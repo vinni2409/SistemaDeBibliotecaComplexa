321b5c556869c540e704007966fd263f0f1a9381
vinni2409 <viniciusfurtado230@gmail.com>
2026-05-21 20:13:48 -0300

    Create Class Livro

--- src/Livro.java
+++ src/Livro.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+public class Livro extends ItemAcervo{
+    private String isbn;
+    private int numeroPaginas;
+    private String genero;
 
-public class Livro {
+    public Livro(int id, String titulo, String autor, int anoDaPublicacao, boolean disponivel, String isbn, int numeroPaginas, String genero) {
+        super(id, titulo, autor, anoDaPublicacao, disponivel);
+        this.isbn = isbn;
+        this.numeroPaginas = numeroPaginas;
+        this.genero = genero;
+    }
 }
