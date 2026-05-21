f6f95c6e38f0c7d07b3ca262af4c8852f4b493d7
vinni2409 <viniciusfurtado230@gmail.com>
2026-05-21 20:55:55 -0300

    Create Class RevistaAcademica

--- src/RevistaAcademica.java
+++ src/RevistaAcademica.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+public class RevistaAcademica extends ItemAcervo{
+    private String issn;
+    private String areaConhecimento;
+    private int edicao;
 
-public class RevistaAcademica {
+    public RevistaAcademica(int id, String titulo, String autor, int anoDaPublicacao, boolean disponivel, String issn, String areaConhecimento, int edicao) {
+        super(id, titulo, autor, anoDaPublicacao, disponivel);
+        this.issn = issn;
+        this.areaConhecimento = areaConhecimento;
+        this.edicao = edicao;
+    }
+
+    public String getDetalhes(){
+        return "ISSN: " + issn + " | Area de conhecimento: " + areaConhecimento + " | Edição: " + edicao;
+    }
 }
